138ce2a3889971485068dcef19fc08c199979fac
Josselin <julien.josselin75@hotmail.fr>
2024-03-10 19:18:29 +0100

    Edit script.py: refactor (separate into functions)

--- script.py
+++ script.py
@@ -6,20 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def main():
-    chrome_options = Options()
-    # Uncomment to run without opening a browser window
-    # chrome_options.add_argument("--headless")
-    chromedriver_path = './chromedriver'
-
-    service = Service(executable_path=chromedriver_path)
-
-    # Initialize Chrome driver with the specified options and service
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    
-    # Go to Google
-    driver.get('https://www.google.com')
-    
+def reject_cookies(driver):
     # Wait for the "Reject all" cookies button to be clickable and click it
     wait = WebDriverWait(driver, 10)
     refuse_cookies_button = wait.until(
@@ -27,14 +14,23 @@ def main():
     )
     refuse_cookies_button.click()
 
-    # Find the search box
+def search(research, driver):
     search_box = driver.find_element(By.NAME, "q")    
-    # Enter the search query
-    research = "les meilleures destinations de vacances"
     search_box.send_keys(research)
-    # Submit the search
     search_box.send_keys(Keys.RETURN)
     print(f"Research '{research}' done!")
+
+def main():
+    chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    chromedriver_path = './chromedriver'
+    service = Service(executable_path=chromedriver_path)
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get('https://www.google.com')
+    
+    reject_cookies(driver)
+    search("les meilleures destinations de vacances", driver)
 
     driver.quit()
 
